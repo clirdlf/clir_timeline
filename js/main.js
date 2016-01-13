aae4822fef2435e481e12bb4c42595f16d74d9ac
Wayne Graham <wgraham@clir.org>
2016-01-13 08:49:29 -0500

    Initial proof-of-concept timeline

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// the id of the Timeline container (no '#'), and
+// the URL to your JSON data file or Google spreadsheet.
+// the id must refer to an element "above" this code,
+// and the element must have CSS styling to give it width and height
+// optionally, a third argument with configuration options can be passed.
+// See below for more about options.
+timeline = new TL.Timeline('timeline-embed',
+    'https://docs.google.com/spreadsheets/d/1d9C6fDiXymS2ADiP-3oCsXy88fyco9RNEDcrQsUKjVw/pubhtml');
